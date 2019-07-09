8cae3ba60b525cbb3e8c13f0f07455ed97b161f6
fengfeifan <fengfeifan@Megvii.com>
2019-07-09 14:44:58 +0800

    修改整体样式

--- src/app.js
+++ src/app.js
@@ -5,14 +5,95 @@ import {Fragment} from 'react';
 import ToolTip from './components/tooltip';
 import XToolTip from 'tooltip-ff';
 
+const contentStyle = {
+  fontSize: '40px',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+};
+
+const rowAroundStyle = {
+  display: 'flex',
+  margin: '40px',
+  justifyContent: 'space-around',
+};
+const rowBetweenStyle = {
+  display: 'flex',
+  margin: '40px',
+  justifyContent: 'space-between',
+};
+
 ReactDOM.render (
   <Fragment>
-    <XToolTip position={'right'} content={'测试'}>
-      {'我是测试按钮'}
-    </XToolTip>
-    <ToolTip position={'right'} content={'测试'}>
-      {'我是测试按钮'}
-    </ToolTip>
+    <div style={rowAroundStyle}>
+      <ToolTip position={'tl'} content={'测试'}>
+        <div style={contentStyle}>
+          {'TL'}
+        </div>
+      </ToolTip>
+      <ToolTip position={'top'} content={'测试'}>
+        <div style={contentStyle}>
+          {'TOP'}
+        </div>
+      </ToolTip>
+      <ToolTip position={'tr'} content={'测试'}>
+        <div style={contentStyle}>
+          {'TR'}
+        </div>
+      </ToolTip>
+    </div>
+    <div style={rowBetweenStyle}>
+      <ToolTip position={'rt'} content={'测试'}>
+        <div style={contentStyle}>
+          {'RT'}
+        </div>
+      </ToolTip>
+      <ToolTip position={'lt'} content={'测试'}>
+        <div style={contentStyle}>
+          {'LT'}
+        </div>
+      </ToolTip>
+    </div>
+    <div style={rowBetweenStyle}>
+      <ToolTip position={'right'} content={'测试'}>
+        <div style={contentStyle}>
+          {'Right'}
+        </div>
+      </ToolTip>
+      <ToolTip position={'left'} content={'测试'}>
+        <div style={contentStyle}>
+          {'LEFT'}
+        </div>
+      </ToolTip>
+    </div>
+    <div style={rowBetweenStyle}>
+      <ToolTip position={'rb'} content={'测试'}>
+        <div style={contentStyle}>
+          {'RB'}
+        </div>
+      </ToolTip>
+      <ToolTip position={'lb'} content={'测试'}>
+        <div style={contentStyle}>
+          {'LB'}
+        </div>
+      </ToolTip>
+    </div>
+    <div style={rowAroundStyle}>
+      <ToolTip position={'bl'} content={'测试'}>
+        <div style={contentStyle}>
+          {'BL'}
+        </div>
+      </ToolTip>
+      <ToolTip position={'bottom'} content={'测试'}>
+        <div style={contentStyle}>
+          {'BOTTOM'}
+        </div>
+      </ToolTip>
+      <ToolTip position={'br'} content={'测试'}>
+        <div style={contentStyle}>
+          {'BR'}
+        </div>
+      </ToolTip>
+    </div>
   </Fragment>,
   document.getElementById ('app')
 );
--- src/components/tooltip/index.js
+++ src/components/tooltip/index.js
@@ -2,20 +2,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './index.scss';
 
+const defaultArray = [
+  'top',
+  'tl',
+  'tr',
+  'left',
+  'lt',
+  'lb',
+  'right',
+  'rt',
+  'rb',
+  'bottom',
+  'bl',
+  'br',
+];
+
 class ToolTip extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      position: this.props.position || 'right',
+      position: defaultArray.includes (this.props.position)
+        ? this.props.position
+        : 'right',
       content: this.props.content,
     };
   }
 
   render () {
     return (
-      <span className={`x-tooltip ${this.props.className}`}>
-        <span className="top">{this.props.children}</span>
-        <span className={`box box-${this.props.position}`}>
+      <span
+        className={`tooltip-ff tooltip-ff-${this.state.position} ${this.props.className}`}
+      >
+        <span className="content">{this.props.children}</span>
+        <span className={`box box-${this.state.position}`}>
           {this.state.content}
         </span>
       </span>
@@ -24,7 +43,20 @@ class ToolTip extends React.Component {
 }
 
 ToolTip.propTypes = {
-  position: PropTypes.oneOf (['top', 'left', 'right', 'bottom']),
+  position: PropTypes.oneOf ([
+    'top',
+    'tl',
+    'tr',
+    'left',
+    'lt',
+    'lb',
+    'right',
+    'rt',
+    'rb',
+    'bottom',
+    'bl',
+    'br',
+  ]),
 };
 
 ToolTip.defaultProps = {
